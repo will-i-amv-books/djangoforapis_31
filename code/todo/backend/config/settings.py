ae7665bd9f77543ae67addb47e705df011ec6847
William <68653853+will-i-amv@users.noreply.github.com>
2023-10-03 16:52:17 -0300

    Add starter config for DRF

--- code/todo/backend/config/settings.py
+++ code/todo/backend/config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+
     # Local
     'todos',
 ]
@@ -121,3 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# rest-framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
